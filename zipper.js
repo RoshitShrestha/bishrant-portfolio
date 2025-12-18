8e3486da68a4dc0349906813d9f8d2040991a846
RoshitShrestha <roshitshrestha@gmail.com>
2025-12-18 23:52:22 +0545

    added console log2

--- zipper.js
+++ zipper.js
@@ -27,6 +27,8 @@ function handleResize() {
   peel = null;
 
   const peelEl = document.querySelector("#book");
+
+  console.log("github code");
   
   const rect = peelEl.getBoundingClientRect();
 
